e0d77ee5a4824fee219d871077f4e74575c3cbc4
GreensCH <changqiaojia@163.com>
2022-01-24 22:43:11 +0800

    Site updated: 2022-01-24 22:42:57

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+hexo.extend.injector.register('body_end', `
+  <div id="aplayer"></div>
+  <link defer rel="stylesheet" href="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.css" />
+  <script src="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.js"></script>
+  <script defer src="/js/aplayer.js"></script>
+`);
+
+
+!(function() {
+    var oldLoadAp = window.onload;
+    window.onload = function () {
+      oldLoadAp && oldLoadAp();
+  
+      new APlayer({
+        container: document.getElementById('aplayer'),
+        fixed: true,
+        autoplay: false,
+        loop: 'all',
+        order: 'random',
+        theme: '#b7daff',
+        preload: 'none',
+        audio: [
+          {
+            name: 'シャナ',
+            artist: 'Guiano,IA',
+            url: '/music/Guiano,IA - シャナ.mp3',
+            cover: '/music/シャナ.jpg'
+          }
+        ]
+      });
+    }
+  })();
