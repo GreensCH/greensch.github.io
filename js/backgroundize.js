5e5000173db873f9b7e205221ea59d96bd44b9b7
GreensCH <changqiaojia@163.com>
2022-03-10 18:45:11 +0800

    Site updated: 2022-03-10 18:45:02

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -1,9 +1,12 @@
-const bannerContainer = $("#banner");
-const viewBg = $("#web_bg");
-const bannerMask = $("#banner .mask");
-const bg = $(bannerContainer).css("background-image");
-$(viewBg).css("background-image", bg);//'url()'
-$(bannerContainer).css("background-image", "url()");
-const color = $(bannerMask).css("background-color");
-$(bannerMask).css("background-color", `rgba(0,0,0,0)`);
-$(viewBg).css("background-color", color);
+if("undefined" == typeof bannerContainer){ 
+    const bannerContainer = $("#banner");
+    const viewBg = $("#web_bg");
+    const bannerMask = $("#banner .mask");
+    const bg = $(bannerContainer).css("background-image");
+    $(viewBg).css("background-image", bg);//'url()'
+    $(bannerContainer).css("background-image", "url()");
+    const color = $(bannerMask).css("background-color");
+    $(bannerMask).css("background-color", `rgba(0,0,0,0)`);
+    $(viewBg).css("background-color", color);
+  }
+
--- js/backgroundize_homepage.js
+++ js/backgroundize_homepage.js
@@ -20,7 +20,7 @@ image_list.forEach((item,index,image_list)=>{
 })
 
 //以下代码是随机切换图片的代码
-var times = 1;
+var times = randomTimes(-1);
 var image_ready = 1;
 const image_fade_time = 1500;
 const image_switch_time = 20000;
@@ -33,13 +33,6 @@ function randomTimes(_times){
     }
         
 }
-setInterval(function () {
-    if(image_ready){
-        times = randomTimes(times)
-        changeBg(times);
-        console.log(times);
-    }
-}, image_switch_time);
 //以下代码是顺序切换图片的代码
 // setInterval(function () {
 //     if(image_ready){
@@ -54,6 +47,15 @@ setInterval(function () {
 //     }
 // }, image_switch_time);
 
+
+setInterval(function () {
+    if(image_ready){
+        times = randomTimes(times)
+        changeBg(times);
+        console.log(times);
+    }
+}, image_switch_time);
+
 function changeBg(times)
 {
     image_ready=0;
@@ -67,7 +69,7 @@ const bannerContainer = $("#banner");
 const viewBg = $("#web_bg");
 const bannerMask = $("#banner .mask");
 const bg = $(bannerContainer).css("background-image");
-$(viewBg).css("background-image", url_image_list[0]);//'url()'
+$(viewBg).css("background-image", url_image_list[times]);//'url()'
 $(bannerContainer).css("background-image", "url()");
 const color = $(bannerMask).css("background-color");
 $(bannerMask).css("background-color", `rgba(0,0,0,0)`);
